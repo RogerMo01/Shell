122fa70c1b26b2e74be95c39ee0af31e6197538b
Roger <morenoroger001@gmail.com>
2023-04-14 16:56:34 -0400

    feat: Print more in one row

--- main.c
+++ main.c
@@ -148,21 +148,42 @@ void ls(char** args, int position)
         if(d == NULL) printf("error: Cannot open directory '%s'\n", cwd);
 
         // Gets the max len of files names
-        // int max = 0;
-        // while((dir_ent = readdir(d)) != NULL)
-        // {
-        //     int current = (int)strlen(dir_ent->d_name);
-        //     if(current > max) max = current;
-        // }
-
-        // Print all files
-        // int col = 0;
-        // int line = 0;
+        int largest = 0;
         while((dir_ent = readdir(d)) != NULL)
         {
             if(dir_ent->d_name[0] == '.') continue; // ignore dot start files
-            printf("%s\n", dir_ent->d_name);
+            int current = (int)strlen(dir_ent->d_name);
+            if(current > largest) largest = current;
         }
+        closedir(d);
+        d = opendir(cwd);
+        
+
+        // Print all files
+        int lineMaxLen = 100;
+        int countInLine = lineMaxLen/(largest+2);
+        int countInCol = 0;
+
+        while((dir_ent = readdir(d)) != NULL)
+        {
+            if(dir_ent->d_name[0] == '.') continue; // ignore dot start files
+            
+            if(countInCol == countInLine)
+            {
+                printf("\n");
+                countInCol = 0;
+            }
+
+            int currentLen = (int)strlen(dir_ent->d_name);
+            int r = largest+2-currentLen;
+            // print dirname
+            printf("%s", dir_ent->d_name);
+            // print r spaces
+            for (int i = 0; i < r; i++) printf(" ");
+
+            countInCol++;
+        }
+        printf("\n");
 
         closedir(d);
     }
