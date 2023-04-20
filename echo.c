fa21cb153ff1545ad2f55ebfe1a363e3580ac2ec
Roger <morenoroger001@gmail.com>
2023-04-19 21:57:10 -0400

    feat: Add functionality
    - Fix print bugs

--- echo.c
+++ echo.c
@@ -6,11 +6,30 @@
 
 int main(int argc, char *argv[])
 {
-    if(argv[1] == NULL){
-       printf("\n"); 
-    } 
-    else{
-        printf("%s\n", argv[1]);
+    // argv[0] - Text
+    // argv[1] - (-1, i>0) rdIn
+    // argv[2] - isPipe
+
+    if(strcmp(argv[1], "-1") == 0)
+    {
+        if(argv[0] == NULL || strcmp(argv[0], ">") == 0 || strcmp(argv[0], ">>") == 0)
+        {
+            if(strcmp(argv[2], "y") == 0) { printf(" "); } else{ printf(" \n"); }
+        }
+        else
+        {
+            printf("%s", argv[0]);
+            if(strcmp(argv[2], "n") == 0) { printf("\n"); }
+        }
     }
+    else
+    {
+        char str[64];
+        fgets(str, 64, stdin);
+
+        printf("%s", str);
+        if(strcmp(argv[2], "n") == 0) { printf("\n"); }
+    }
+
     return 1;
 }
